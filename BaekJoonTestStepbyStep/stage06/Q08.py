ee00e1a7de93ed47c857e41a40c5a50278207693
Kategorie <kkt4723@naver.com>
2023-08-14 02:08:35 +0900

    0814-2

---
+++ BaekJoonTestStepbyStep/stage06/Q08.py
@@ -0,0 +1,8 @@
+className = 0
+grade = 0
+level = 0
+dic = {0} #과목명, 학점, 등급
+S = ['aaa', 3.0, 'A+'], ['bbb', 3.0, 'A+'], ['ccc', 3.0, 'A0'], ['ddd', 3.0, 'A+'], ['eee', 3.0, 'A+'], ['fff', 3.0, 'B0'], ['ggg', 3.0, 'A0'], ['hhh', 3.0, 'B0'], 
+
+for i in range(20):
+    S = list(map(input().split()))
