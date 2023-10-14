d576dae09f0291d0fdeb1efaa869449c97cc91b0
Kategorie <kkt4723@naver.com>
2023-10-15 02:18:37 +0900

    1015-2

--- BaekJoonTestStepbyStep/stage10/Q03.py
+++ BaekJoonTestStepbyStep/stage10/Q03.py
@@ -3,11 +3,10 @@ A1, A2 = map(int, input().split())
 B1, B2 = map(int, input().split())
 C1, C2 = map(int, input().split())
 
-for i in range(1,3):
-if A1 == B1:
+
+    if A1 == B1:
         pass
-elif B1 == C1:
+    elif B1 == C1:
         pass
     else: # A1 == C1
         pass
-
