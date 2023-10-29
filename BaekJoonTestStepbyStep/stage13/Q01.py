ccdae67bee90b375f1ae4556e24819dd908d4dd4
Kategorie <kkt4723@naver.com>
2023-10-29 20:47:08 +0900

    1029

--- BaekJoonTestStepbyStep/stage13/Q01.py
+++ BaekJoonTestStepbyStep/stage13/Q01.py
@@ -1,4 +1,33 @@
+
 # 삽입정렬
+N = int(input())
+list_A = []
 
+for i in range(N):
+    list_A.append(int(input()))
 
+for j in range(N - 1):
+    for k in range(N - 1 - j):
+
+for x in range(len(list_A)):
+    print(list_A[x])
+"""
 # 버블정렬
+N = int(input())
+list_A = []
+
+for i in range(N):
+    list_A.append(int(input()))
+
+for j in range(N - 1):
+    for k in range(N - 1 - j):
+        if list_A[k + 1] < list_A[k]:
+            tmp = list_A[k]
+            list_A[k] = list_A[k + 1]
+            list_A[k + 1] = tmp
+        else:
+            continue
+
+for x in range(len(list_A)):
+    print(list_A[x])
+"""
