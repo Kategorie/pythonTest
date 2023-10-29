d780dc0fcd52c848b421619e0005920e81ca126b
Kategorie <kkt4723@naver.com>
2023-10-29 21:29:42 +0900

    1029_2

--- BaekJoonTestStepbyStep/stage13/Q01.py
+++ BaekJoonTestStepbyStep/stage13/Q01.py
@@ -6,8 +6,11 @@ list_A = []
 for i in range(N):
     list_A.append(int(input()))
 
-for j in range(N - 1):
-    for k in range(N - 1 - j):
+for j in range(1, N): # 현재값
+    for k in range(j): # 비교할 정렬값
+        if list_A[k] < list_A[j]:
+            continue
+        else
 
 for x in range(len(list_A)):
     print(list_A[x])
