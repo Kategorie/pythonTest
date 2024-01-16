44be98837394db0522e1060c3fe5365d94f2e90d
Kategorie <kkt4723@naver.com>
2024-01-16 21:55:10 +0900

    1016

--- BaekJoonTestStepbyStep/stage15/Q03.py
+++ BaekJoonTestStepbyStep/stage15/Q03.py
@@ -1,6 +1,25 @@
-a, b = map(int, input().split())  # 분자 분모
-c, d = map(int, input().split())
+def euclideean(small, big):
+    res = big % small
+    if res == 0:
+        return small
+    else:
+        return euclideean(small, res)
 
+
+if __name__ == "__main__":
+    a, b = map(int, input().split())  # 분자 분모
+    c, d = map(int, input().split())
+    # a c 2 3
+    # b d 7 5
+    A = a * d
+    B = b * d  # B = D
+    C = c * b
+
+    res1 = A + C
+    res2 = B
+    euclideean(res1, res2)
+
+    print(int(res1), int(res2))
 
 """
 # 알고리즘은 맞는데 시간 초과됨
