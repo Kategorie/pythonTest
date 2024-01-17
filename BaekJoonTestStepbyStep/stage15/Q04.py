8abe23840f54318cc471df5bd0766c0e84deabd5
Kategorie <kkt4723@naver.com>
2024-01-17 14:24:45 +0900

    1017

--- BaekJoonTestStepbyStep/stage15/Q04.py
+++ BaekJoonTestStepbyStep/stage15/Q04.py
@@ -1,22 +1,20 @@
 N = int(input())
 
-
 L = []
 tmp1 = 0
 tmp2 = 0
 num = 1000000000
 flag = True
-for i in range(N - 1):
+for i in range(N):
     L.append(int(input()))
-    if flag: 
-        flag = False 
+    if flag:
+        flag = False
         continue
     else:
-        res = L[i-1] - L[i]
+        res = L[i] - L[i - 1]
         if res < num:
             num = res
 
-S = num * j for j in range(2, N - 1))
-
-
-print()
+cnt = ((L[len(L) - 1] - L[0]) // num) + 1
+ans = cnt - len(L)
+print(ans)
