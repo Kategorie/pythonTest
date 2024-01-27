26e54907f331647a9c53332bc870725ebb6d29e5
Kategorie <kkt4723@naver.com>
2024-01-27 14:36:15 +0900

    1027

--- BaekJoonTestStepbyStep/stage15/Q05.py
+++ BaekJoonTestStepbyStep/stage15/Q05.py
@@ -1,3 +1,35 @@
+# 특정값 n을 기준으로 반드시 n의 제곱근 내의 범위에 소수가 1개는 꼭 있다. - 정수론 -
+def Eratos(data):
+    if data == 0 or data == 1 or data == 2:
+        return 2
+    elif data == 3:
+        return 3
+    else:
+        for i in range(data, data * 2):
+            flag = False
+            for j in range(2, int(data**0.5) + 2):
+                if i % j == 0:
+                    flag = False
+                    break
+                else:
+                    flag = True
+            if flag:
+                return i
+
+
+if __name__ == "__main__":
+    N = int(input())
+    res = []
+    for _ in range(N):
+        tmp = int(input())
+        res.append(Eratos(tmp))
+
+    for _ in range(len(res)):
+        print(res[_])
+
+"""
+# 40억개 계산량 때문에 메모리 초과됨
+# 다른 방법 사용 필요함 코드도 최적화는 아님
 def Eratos(data, num):
     prime = []
     num = num * num + 1
@@ -36,4 +68,4 @@ for x in range(N):
 # 2. 현재 값을 기준으로 prime 값의 배수를 제거해 준다.
 # 3. 1은 제외하고 2부터 배수를 찾는데 대신 2는 소수로 추가한다.
 # 4. 주어진 값에서 가장 큰 수를 기준으로 한번 다 구한뒤 찾아서 하는 것과 매번 계산하는 것 중 뭐가 더 빠를지 고민해 봐야할듯
-# 40억개 계산
+"""
