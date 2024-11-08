fb77d309f7a2b8d858dea4d395f75f784d4daa4c
Kategorie <kkt4723@naver.com>
2024-11-08 21:24:53 +0900

    check

--- BaekJoonTestStepbyStep/stage18/Q05.py
+++ BaekJoonTestStepbyStep/stage18/Q05.py
@@ -1,30 +1,101 @@
 import sys
 import collections
 
+"""
 N, M = map(int, sys.stdin.readline().split())
 data = []
 for _ in range(N):
-    data.append(sys.stdin.readline().rstrip())
+    input_ = sys.stdin.readline().rstrip()
+    if M <= len(input_):
+        data.append(input_)
+"""
+N = 12
+M = 4
+data = [
+    "abbbb",
+    "abbbb",
+    "abbbb",
+    "acccc",
+    "acccc",
+    "acccc",
+    "aaaaa",
+    "aaaaa",
+    "aaaaa",
+    "adddd",
+    "adddd",
+    "adddd",
+]
 
-tmp_D = []
-for i in range(N):
-    if M <= len(data[i]):
-        tmp_D.append(data[i])
+res = []
+data_c = collections.Counter(data)
+s_data_c = sorted(data_c.items(), key=lambda x: x[1], reverse=True)
+tmp = []
+for i in range(1, len(s_data_c)):
+    if s_data_c[i - 1][1] == s_data_c[i][1]:  # 빈도 수가 같을 때,
+        tmp.append(s_data_c[i - 1][0])
+    else:  # 빈도 수가 다를 때,
+        if tmp:  # 빈도 수가 같은 경우의 리스트를 문자열 길이로 정렬
+            tmp.append(s_data_c[i - 1][0])
+            tmp = sorted(tmp, key=len, reverse=True)
+            res += tmp
+            tmp = []
+        else:
+            res.append(s_data_c[i - 1][0])
 
-res1 = []
-res2 = []
-tmp_D_c = collections.Counter(tmp_D)
-tmp_D_c_k = list(tmp_D_c.keys())
-tmp_D_c_v = list(tmp_D_c.values())
-t = sorted(tmp_D_c_v)
-for j in range(1, len(tmp_D_c_v)):
-    if t[j - 1] == t[j]:
-        res1.append(tmp_D_c_k[j - 1])
-    else:
-        res2.append(tmp_D_c_k[j - 1])
+if tmp:  # 빈도 수가 같은 경우로 반복이 종료될 경우,
+    tmp.append(s_data_c[i][0])  # 마지막 요소 추가
+    tmp = sorted(tmp, key=len, reverse=True)
+    res += tmp
+    tmp = []
+else:  # 빈도 수가 다른 경우로 반복이 종료될 경우,
+    res.append(s_data_c[i][0])  # 마지막 요소 추가
 
-res1.sort(key=lambda x: len(x))
-res2.sort(key=lambda x: len(x))
+for r in res:
+    print(r)
 
-for y in res1 + res2:
-    print(y)
+"""
+N = 12
+M = 5
+data = [
+    "appearance",
+    "append",
+    "attendance",
+    "swim",
+    "swift",
+    "swift",
+    "swift",
+    "mouse",
+    "wallet",
+    "mouse",
+    "ice",
+    "age",
+]
+"""
+"""
+N = 7
+M = 4
+data = [
+    "apple",
+    "ant",
+    "sand",
+    "apple",
+    "append",
+    "sand",
+    "sand",
+]
+"""
+"""
+N = 9
+M = 4
+data = [
+    "apple",
+    "attend",
+    "list",
+    "attend",
+    "attend",
+    "apple",
+    "apple",
+    "create",
+    "garage",
+]
+"""
